ba83d40737dd80b3b7988ae2aa4e242a05e8dbc8
Zdravko Lucic <zdravko.lucic@doubleslash.de>
2020-12-20 21:48:56 +0100

    some updates

---
+++ 03 RestWithSpringBootUdemy Scaffold Person/src/main/java/de/zlucic/services/PersonServices.java
@@ -0,0 +1,67 @@
+package de.zlucic.services;
+
+import de.zlucic.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    ArrayList<Person> persons = new ArrayList<>();
+
+    public PersonServices() {
+        setupMock();
+    }
+
+    private void setupMock() {
+        persons.add( new Person( counter.incrementAndGet(), "Zdravko", "Lucic", "Mozarstr. 37/1, 88255 Sigmaringen", "male"));
+        persons.add( new Person( counter.incrementAndGet(), "Zlatko", "Lucic", "Bittelschießerstr. 38, 72488 Sigmaringen", "male"));
+    }
+
+    public Person findById(Long id) {
+        for( Person p:persons) {
+            if( p.getId().equals(id)) return p;
+        }
+        return null;
+    }
+
+    public List<Person> findAll() {
+        return persons;
+    }
+
+    public Long addPerson( Person person) {
+        Long id = counter.incrementAndGet();
+        person.setId( id);
+        persons.add( person);
+        return id;
+    }
+
+    public String updatePerson( Person person) {
+        for( Person p:persons) {
+            if( p.getId().equals( person.getId())) {
+                p.setFirstName( person.getFirstName());
+                p.setLastName( person.getLastName());
+                p.setAddress( person.getAddress());
+                p.setGender( person.getGender());
+                return "Updated person: " + p.toString();
+            }
+        }
+        return "Person with id " + person.getId() + " not found";
+    }
+
+    public String deletePerson( Long id) {
+        for( int index = 0; index<persons.size(); index++) {
+            if( persons.get( index).getId().equals(id)) {
+                String person = persons.get( index).toString();
+                persons.remove( index);
+                return "Removed person: " + person;
+            }
+        }
+        return "Person with id " + id + " not found";
+    }
+}
